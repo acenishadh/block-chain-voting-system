259ae7f55c1a88bfd7b431dc1af0fce6adb96b09
Mohammed Nishadh S <151752464+acenishadh@users.noreply.github.com>
2025-09-15 09:02:59 +0530

    fourth commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,11 @@
 const hre = require("hardhat");
 
 async function main() {
-  const parties = ["Party A", "Party B", "Party C"]; // initial parties
+  const parties = ["Party A", "Party B", "Party C"]; // all parties
   const Voting = await hre.ethers.getContractFactory("Voting");
   const voting = await Voting.deploy(parties);
   await voting.deployed();
-  console.log("Voting contract deployed to:", voting.address);
+  console.log("Voting contract deployed to:", voting.address); //prints the contract adress, paste this in app.js
 }
 
 main().catch((error) => {
